39785072cb0042c7fcede04bdaa82e27a83ad49f
Samuel Mercado <samuel.mg@gmx.com>
2025-03-19 16:16:45 -0600

    Crea MensajeSeeder y lo llama desde DatabaseSeeder

--- database/seeders/MensajeSeeder.php
+++ database/seeders/MensajeSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Mensaje;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class MensajeSeeder extends Seeder
 {
@@ -12,6 +14,18 @@ class MensajeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Mensaje::factory()->count(50)->create();
+
+        Mensaje::make([
+            'nombre' => 'Juan',
+            'correo' => 'algo',
+            'mensaje' => 'Hola',
+        ]);
+
+        DB::table('mensajes')->insert([
+            'nombre' => 'Juan',
+            'correo' => 'algo@asdf.com',
+            'mensaje' => 'Hola',
+        ]);
     }
 }
